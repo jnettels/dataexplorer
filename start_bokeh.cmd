65b83f3317bce62a206fbd213a2718a5f0a7d9c5
Joris Nettelstroth <joris.nettelstroth@stw.de>
2017-11-16 10:14:46 +0100

    Batch file: Improve visual output

--- start_bokeh.cmd
+++ start_bokeh.cmd
@@ -1,8 +1,16 @@
-@echo Welcome! This batch file finds your IP and starts a Bokeh server that can be accessed remotely.
+@echo off
 
 for /f "tokens=1-2 delims=:" %%a in ('ipconfig^|find "IPv4"') do set ip=%%b
 set ip=%ip:~1%
 
-bokeh serve dataexplorer.py --show --allow-websocket-origin localhost:80 --allow-websocket-origin %ip%:80 --port 80
+echo This batch file starts a Bokeh server that can be accessed remotely.
+echo For local access type "localhost/dataexplorer" in your browser.
+echo For remote access type "%IP%/dataexplorer" in your browser.
+
+bokeh serve dataexplorer.py --show --allow-websocket-origin localhost:80 ^
+ --allow-websocket-origin %ip%:80 --port 80 ^
+ --log-level info
+
+rem 'log-level' can be one of: trace, debug, info, warning, error or critical
 
 pause
