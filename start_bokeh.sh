6ef71f73fb70cf91eacb9a7ffa2205566a5cf296
Joris Nettelstroth <joris.nettelstroth@stw.de>
2018-02-09 09:02:28 +0000

    Adapt start_bokeh.sh to Raspberry Pi

--- start_bokeh.sh
+++ start_bokeh.sh
@@ -1,9 +1,12 @@
 #! /bin/sh
 
-echo This shell script starts a Bokeh server on Linux systems
-ip=$(hostname -I)
-port=80
+echo This shell script starts a Bokeh server on the Raspberry Pi
+ip=$(hostname -I)  # Get the current IP address
+ip=${ip%?}  # Remove the last character of the string
+port=80  # Set a port (80 requires root rights)
 loglevel="info"
+N=3  # Number of worker processes
 echo For remote access type $ip:$port/dataexplorer in remote browser
-nohup bokeh serve ../dataexplorer --allow-websocket-origin $ip:$port \
-    --port $port --log-level $loglevel > dataexplorer.log
+nohup /home/pi/berryconda3/bin/bokeh serve /home/pi/dataexplorer \
+    --allow-websocket-origin $ip:$port --port $port --num-procs $N \
+    --log-level $loglevel > /home/pi/dataexplorer/dataexplorer.log
