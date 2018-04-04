jnettels/dataexplorer

commit 36f94226c94407cd3f9b19c0f73443f15be23029

Only update selection of the correlation heat map if necessary

dataexplorer.py
@@ -119,7 +119,7 @@ class Dataexplorer(object):
 
     def __init__(self, df, data_name, server_mode, combinator=0, vals_max=6,
                  window_height=974, window_width=1920,
-                 output_backend='canvas'):
+                 output_backend='webgl'):
         '''Return a Dataexplorer object, the object containing all the session
         information. Initialize all object properties.
         Perform all the tasks necessary to create the Data Explorer user
@@ -160,6 +160,7 @@ class Dataexplorer(object):
         self.p_w = 250  # global setting for plot_width
         self.load_mode_append = 0  # 0 equals False equals replace
         self.selected_figs = []
+        self.hm_sel_ids = []
         self.window_height = window_height  # Pixels of browser window
         self.window_width = window_width  # Pixels of browser window
         self.output_backend = output_backend
@@ -1279,8 +1280,10 @@ def update_hm_source_selected(DatEx):
     figs_all = list(itertools.product(DatEx.vals_active, repeat=2))
     figs_all_s = pd.Series(range(len(figs_all)), index=figs_all)
     figs_sel = get_combinations(DatEx)
-    DatEx.hm_sel_ids = figs_all_s[figs_sel].tolist()
-    DatEx.hm_source.selected = Selection(indices=DatEx.hm_sel_ids)
+    hm_sel_ids = figs_all_s[figs_sel].tolist()
+    if hm_sel_ids != DatEx.hm_sel_ids:  # Check if update is necessary
+        DatEx.hm_sel_ids = hm_sel_ids
+        DatEx.hm_source.selected = Selection(indices=DatEx.hm_sel_ids)
 
 
 def callback_tabs(attr, old, new, DatEx):
@@ -1354,7 +1357,7 @@ def callback_heatmap(attr, old, new, DatEx):
     Return:
         None
     '''
-    DatEx.hm_sel_ids = new['1d']['indices']
+    DatEx.hm_sel_ids = new.indices
     figs_all = pd.Series(list(itertools.product(DatEx.vals_active, repeat=2)))
     if DatEx.hm_sel_ids == []:  # Happens when clicking the reset button
         DatEx.selected_figs = figs_all

commit 696eaf1064edb90148d61530de29d300723f0858

Fix bug introduced in previous commit 36f9422

dataexplorer.py
@@ -731,7 +731,7 @@ def create_corr_matrix_heatmap(self):
     # Call a custom function to create a heatmap figure
     self.corr_matrix_heatmap = create_heatmap(corr_matrix, self)
     # Update the selection immediately
-    update_hm_source_selected(self)
+    update_hm_source_selected(self, force=True)
     # Whenever the selection on the matrix changes, this callback happens
     self.hm_source.on_change('selected', partial(callback_heatmap, DatEx=self))
 
@@ -1265,7 +1265,7 @@ def update_combinator(attr, old, new, DatEx):
     DatEx.grid_needs_update = True
 
 
-def update_hm_source_selected(DatEx):
+def update_hm_source_selected(DatEx, force=False):
     '''Update the selection of the heatmap source. We create a Pandas Series
     from the full list of possible selections. We filter it with the currently
     selected figures. The result is a list of the indices that we can pass to
@@ -1274,6 +1274,8 @@ def update_hm_source_selected(DatEx):
     Args:
         DatEx (Dataexplorer): The object containing all the session information
 
+        force (bool, optional): Force update of selection
+
     Returns:
         None
     '''
@@ -1281,7 +1283,7 @@ def update_hm_source_selected(DatEx):
     figs_all_s = pd.Series(range(len(figs_all)), index=figs_all)
     figs_sel = get_combinations(DatEx)
     hm_sel_ids = figs_all_s[figs_sel].tolist()
-    if hm_sel_ids != DatEx.hm_sel_ids:  # Check if update is necessary
+    if hm_sel_ids != DatEx.hm_sel_ids or force is True:  # Is update necessary?
         DatEx.hm_sel_ids = hm_sel_ids
         DatEx.hm_source.selected = Selection(indices=DatEx.hm_sel_ids)
 